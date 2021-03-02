a54378af6d8b8778ac8af876843ae8defef23ab0
doaa-1996 <doaaobeidat1996@gmail.com>
2021-03-02 10:53:41 +0200

    edit 2 bus mall

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 'use strict';
-
 let busMallArray = ['bag','banana','bathroom','boots','breakfast',
   'bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep',
   'scissors','shark','sweep','tauntaun','unicorn','water-can','wine-glass'];
-
-
 const imageSection = document.getElementById ( 'imageSection' );
 const leftImage = document.getElementById( 'leftImage' );
 const middleImage = document.getElementById( 'middleImage' );
 const rightImage = document.getElementById( 'rightImage' );
-
 let leftImgIndex = 0;
 let middleImgIndex = 0;
 let rightImgIndex = 0;
 const clickCounter = 25;
-
-
-
 function Bussmall( name ) {
   this.name = name;
   this.image = `./img/${name}.jpg`;
@@ -24,169 +17,103 @@ function Bussmall( name ) {
   this.shown = 0;
   Bussmall.all.push( this );
 }
-
-
 Bussmall.all = [];
 Bussmall.counter = 0;
-
-
 for( let i = 0; i < busMallArray.length; i++ ) {
   new Bussmall( busMallArray[i] );
 }
-
-
-
-
-
 // let left = 100;
 // let middle = 100;
 // let right = 100;
-
 // function render() {
-
 //   let leftIndex = randomNumber( 0, Bussmall.all.length - 1 );
-
 //   while( leftIndex === left || leftIndex === middle || leftIndex === right ){
 //     leftIndex = randomNumber ( 0, Bussmall.all.length - 1 );
 //   }
-
 //   leftImage.src = Bussmall.all[leftIndex].image;
 //   leftImage.alt = Bussmall.all[leftIndex].name;
 //   leftImgIndex = leftIndex;
-
 //   let middleIndex;
 //   let rightIndex;
-
-
 //   rightIndex = randomNumber( 0, Bussmall.all.length - 1 );
-
 //   while ( rightIndex === leftIndex || rightIndex === left || rightIndex === middle || rightIndex === right );
 //   { rightIndex = randomNumber( 0, Bussmall.all.length - 1 );
 //   }
-
 //   rightImage.src = Bussmall.all[rightIndex].image;
 //   rightImage.alt = Bussmall.all[rightIndex].name;
 //   rightImgIndex = rightIndex;
-
-
-
 //   middleIndex = randomNumber( 0, Bussmall.all.length - 1 );
-
 //   while ( middleIndex === leftIndex || middleIndex === rightIndex || middleIndex === left || middleIndex === middle || middleIndex === right );{
 //     middleIndex = randomNumber( 0, Bussmall.all.length - 1 );
-
 //   }
-
 //   middleImage.src = Bussmall.all[middleIndex].image;
 //   middleImage.alt = Bussmall.all[middleIndex].name;
 //   middleImgIndex = middleIndex;
-
-
-
-
-
-
 //   Bussmall.all[leftIndex].shown++;
 //   Bussmall.all[middleIndex].shown++;
 //   Bussmall.all[rightIndex].shown++;
-
 //   left = leftIndex;
 //   middle = middleIndex;
 //   right = rightIndex;
-
 // }
-
-
-
-
-
+let arr = [30,30,30 ];
 function render() {
-  let leftIndex = randomNumber( 0, Bussmall.all.length - 1 );
+  let leftIndex = randomNumber ( 0, Bussmall.all.length - 1 );
+  while ( leftIndex === arr[0] || leftIndex === arr[1] || leftIndex === arr[2] ){
+    leftIndex = randomNumber( 0, Bussmall.all.length - 1 ); }
   leftImage.src = Bussmall.all[leftIndex].image;
   leftImage.alt = Bussmall.all[leftIndex].name;
-
   let middleIndex;
   let rightIndex;
-
   do {
     rightIndex = randomNumber( 0, Bussmall.all.length - 1 );
-  } while( leftIndex === rightIndex );
-
+  } while( leftIndex === rightIndex || rightIndex === arr[0] || rightIndex === arr[1] || rightIndex === arr[2] );
   rightImage.src = Bussmall.all[rightIndex].image;
   rightImage.alt = Bussmall.all[rightIndex].name;
-
-
   do{
     middleIndex = randomNumber( 0, Bussmall.all.length - 1 );
-  } while ( middleIndex === leftIndex || middleIndex === rightIndex );
-
+  } while ( middleIndex === leftIndex || middleIndex === rightIndex || middleIndex === arr[0] || middleIndex === arr[1] || middleIndex === arr[2] );
   middleImage.src = Bussmall.all[middleIndex].image;
   middleImage.alt = Bussmall.all[middleIndex].name;
-
-
   Bussmall.all[leftIndex].shown++;
   Bussmall.all[middleIndex].shown++;
   Bussmall.all[rightIndex].shown++;
-
-
   leftImgIndex = leftIndex;
   rightImgIndex = rightIndex;
   middleImgIndex = middleIndex;
-
+  arr[0] = leftIndex;
+  arr[1] = rightIndex;
+  arr[1] = middleIndex;
 }
-
-
-
-
 function handelClick( event ) {
-
   if( Bussmall.counter < clickCounter ) {
     const clickedElement = event.target;
     if( clickedElement.id === 'leftImage' || clickedElement.id === 'middleImage' || clickedElement.id === 'rightImage' ) {
       if( clickedElement.id === 'leftImage' ) {
         Bussmall.all[leftImgIndex].clicks++;
       }
-
       if( clickedElement.id === 'rightImage' ) {
         Bussmall.all[rightImgIndex].clicks++;
       }
-
       if( clickedElement.id === 'middleImage' ) {
         Bussmall.all[middleImgIndex].clicks++;
       }
-
-
-
       Bussmall.counter++;
       render();
-
       // console.log( Bussmall.all );
     }
-
   }
   // else {
   //   renderChart();
   // }
 }
-
-
 imageSection.addEventListener( 'click', handelClick );
-
-
-
 function randomNumber( min, max ) {
   return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 }
-
 render();
-
-
-
-
-
 const Button = document.getElementById( 'button' );
 Button.addEventListener ( 'click', function (){
-
   const result = document.getElementById( 'results' );
   const ulElement = document.createElement( 'ul' );
   result.appendChild( ulElement );
@@ -198,28 +125,17 @@ Button.addEventListener ( 'click', function (){
   }
 }
 );
-
-
-
-
-
-
-
-
 const chartButton = document.getElementById( 'chartbutton' );
 chartButton.addEventListener ( 'click',renderChart ) ;
-
 function renderChart (){
   let nameArray = [];
   let clicksArray = [];
   let showsArray = [];
-
   for ( let i = 0; i < Bussmall.all.length; i++ ) {
     nameArray.push ( Bussmall.all[i].name );
     clicksArray.push ( Bussmall.all[i].clicks );
     showsArray.push( Bussmall.all[i].shown );
   }
-
   let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
   new Chart ( ctx, {
     type: 'bar',
@@ -249,7 +165,6 @@ function renderChart (){
           'rgba(153, 102, 255, 0.2)',
           'rgba(255, 159, 64, 0.2)',
           'rgba(255, 99, 132, 0.2)'
-
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
@@ -335,18 +250,3 @@ function renderChart (){
     }
   } );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
